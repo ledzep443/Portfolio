42eb37a18a94664f6766ea1d8679d97015593abe
Jimmy Purnell <78944509+ledzep443@users.noreply.github.com>
2021-06-19 09:06:09 -0400

    Added Blog and Projects models

---
+++ Jimmy_Purnell_Development/Models/Blog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Jimmy_Purnell_Development.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Post { get; set; }
+    }
+}
---
+++ Jimmy_Purnell_Development/Models/Projects.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Jimmy_Purnell_Development.Models
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string GitHub { get; set; }
+    }
+}
